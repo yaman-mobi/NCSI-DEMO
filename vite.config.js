45325bb36bb6a8185a420e6350894b4e330ed47b
Mustafa Alyousef <Mustafa.alyousef@Realsoft-me.com>
2026-03-24 12:02:35 +0000

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'production' ? '/ncsi_demo/' : '/',
+ base: '/ncsi_demo/',
   server: {
     headers: {
       'Content-Security-Policy': 'frame-ancestors *',
